59affead71f1bf8ff327df1cad86839825ffabba
yunifp <darrenarmani666@gmail.com>
2026-04-17 09:20:47 +0700

    s

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
+      '/api': 'https://decoded-app-dsg9skc27-yunifps-projects.vercel.app'
     }
   }
 })
